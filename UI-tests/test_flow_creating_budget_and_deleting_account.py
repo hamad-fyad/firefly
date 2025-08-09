a88b029903287fea8ac5f35d5046178130c26592
hamad <Hamad.fyad.05@gmail.com>
2025-08-09 16:32:49 +0300

    updaated

--- UI-tests/test_flow_creating_budget_and_deleting_account.py
+++ UI-tests/test_flow_creating_budget_and_deleting_account.py
@@ -4,20 +4,31 @@ import unittest
 from selenium import webdriver
 from Helper_Class import LoginPage, registerPage,new_user, DashboardPage
 import os 
-
+import tempfile
 firefly = os.environ.get("FIREFLY_URL", "http://52.212.42.101:8080")
 class FireflyBudgetTest(unittest.TestCase):
 
+
+
     def setUp(self):
         self.name = "test_name_" + str(datetime.datetime.now())
-        headless = os.environ.get("HEADLESS", False).lower() == True
+        
+        headless = str(os.environ.get("HEADLESS", "false")).lower() in ("true", "1")
         options = webdriver.ChromeOptions()
+
         if headless:
-            options.add_argument('--headless')
-            options.add_argument("--sandbox")
+            options.add_argument('--headless=new')
+            options.add_argument('--no-sandbox')
+            options.add_argument('--disable-dev-shm-usage')
+
+        # ✅ Unique Chrome user profile
+        user_data_dir = tempfile.mkdtemp()
+        options.add_argument(f"--user-data-dir={user_data_dir}")
+
         self.driver = webdriver.Chrome(options=options)
         self.driver.get(firefly + "/register")
         self.driver.maximize_window()
+
 
     def test_create_budget(self):
     
